08c3c5be71d286b19aa1d616919947ba2549ffb8
Helder Pinhal <helder@notifica.re>
2022-01-20 15:55:27 +0000

    refactor(core): adjust exported plugin name

--- packages/capacitor-notificare/src/index.ts
+++ packages/capacitor-notificare/src/index.ts
@@ -1,10 +1,10 @@
 import { registerPlugin } from '@capacitor/core';
 
-import { NotificarePlugin } from './definitions';
+import type { NotificarePlugin } from './definitions';
 
-const NotificarePlugin = registerPlugin<NotificarePlugin>('NotificarePlugin', {
+const Notificare = registerPlugin<NotificarePlugin>('NotificarePlugin', {
   web: () => import('./web').then((m) => new m.NotificarePluginWeb()),
 });
 
 export * from './definitions';
-export { NotificarePlugin };
+export { Notificare };
