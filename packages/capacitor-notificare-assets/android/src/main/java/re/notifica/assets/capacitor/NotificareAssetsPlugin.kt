a5e43e5f214ea186626de6f78b162f3ce0adc2e3
Helder Pinhal <helder@notifica.re>
2022-01-20 16:07:15 +0000

    refactor: go kotlin!

---
+++ packages/capacitor-notificare-assets/android/src/main/java/re/notifica/assets/capacitor/NotificareAssetsPlugin.kt
@@ -0,0 +1,20 @@
+package re.notifica.assets.capacitor
+
+import android.util.Log
+import com.getcapacitor.annotation.CapacitorPlugin
+import com.getcapacitor.PluginMethod
+import com.getcapacitor.PluginCall
+import com.getcapacitor.JSObject
+import com.getcapacitor.Plugin
+
+@CapacitorPlugin(name = "NotificareAssetsPlugin")
+class NotificareAssetsPlugin : Plugin() {
+    @PluginMethod
+    fun echo(call: PluginCall) {
+        val value = call.getString("value")
+        Log.i("Echo", value!!)
+        val ret = JSObject()
+        ret.put("value", value)
+        call.resolve(ret)
+    }
+}
