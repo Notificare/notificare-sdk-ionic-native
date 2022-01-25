c570f6942bdc8a7f538acb046908852d4be8f8b0
Helder Pinhal <helder@notifica.re>
2022-01-25 11:23:30 +0000

    chore(sample): cleanup unnecessary dependencies

--- packages/sample/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-
-import App from './App';
-
-test('renders without crashing', () => {
-  const { baseElement } = render(<App />);
-  expect(baseElement).toBeDefined();
-});
