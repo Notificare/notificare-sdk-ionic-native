e9f820cc8d14cd8615603c49a7fc977648aa591b
Helder Pinhal <helder@notifica.re>
2022-01-25 11:11:10 +0000

    feat(core): add android native plugin

--- packages/capacitor-notificare/android/src/main/java/re/notifica/capacitor/NotificarePlugin.kt
+++ packages/capacitor-notificare/android/src/main/java/re/notifica/capacitor/NotificarePlugin.kt
@@ -1,20 +1,417 @@
 package re.notifica.capacitor
 
-import android.util.Log
+import android.content.Intent
+import com.getcapacitor.*
 import com.getcapacitor.annotation.CapacitorPlugin
-import com.getcapacitor.PluginMethod
-import com.getcapacitor.PluginCall
-import com.getcapacitor.JSObject
-import com.getcapacitor.Plugin
+import org.json.JSONObject
+import re.notifica.Notificare
+import re.notifica.NotificareCallback
+import re.notifica.ktx.device
+import re.notifica.ktx.events
+import re.notifica.models.*
 
 @CapacitorPlugin(name = "NotificarePlugin")
 public class NotificarePlugin : Plugin() {
-    @PluginMethod
-    public fun echo(call: PluginCall) {
-        val value = call.getString("value")
-        Log.i("Echo", value!!)
-        val ret = JSObject()
-        ret.put("value", value)
-        call.resolve(ret)
+    override fun load() {
+        EventBroker.setup(this::notifyListeners)
+        Notificare.intentReceiver = NotificarePluginIntentReceiver::class.java
+
+        val intent = activity?.intent
+        if (intent != null) handleOnNewIntent(intent)
     }
+
+    override fun handleOnNewIntent(intent: Intent) {
+        // Try handling the test device intent.
+        if (Notificare.handleTestDeviceIntent(intent)) return
+
+        // Try handling the dynamic link intent.
+        val activity = activity
+        if (activity != null && Notificare.handleDynamicLinkIntent(activity, intent)) return
+
+        val url = intent.data?.toString()
+        if (url != null) {
+            EventBroker.dispatchEvent("url_opened", JSObject().apply {
+                put("url", url)
+            })
+        }
+    }
+
+    // region Notificare
+
+    @PluginMethod
+    public fun isConfigured(call: PluginCall) {
+        call.resolve(
+            JSObject().apply {
+                put("result", Notificare.isConfigured)
+            }
+        )
+    }
+
+    @PluginMethod
+    public fun isReady(call: PluginCall) {
+        call.resolve(
+            JSObject().apply {
+                put("result", Notificare.isReady)
+            }
+        )
+    }
+
+    @PluginMethod
+    public fun launch(call: PluginCall) {
+        Notificare.launch()
+        call.resolve()
+    }
+
+    @PluginMethod
+    public fun unlaunch(call: PluginCall) {
+        Notificare.unlaunch()
+        call.resolve()
+    }
+
+    @PluginMethod
+    public fun getApplication(call: PluginCall) {
+        call.resolve(
+            JSObject().apply {
+                put("result", Notificare.application?.toJson())
+            }
+        )
+    }
+
+    @PluginMethod
+    public fun fetchApplication(call: PluginCall) {
+        Notificare.fetchApplication(object : NotificareCallback<NotificareApplication> {
+            override fun onSuccess(result: NotificareApplication) {
+                call.resolve(
+                    JSObject().apply {
+                        put("result", result.toJson())
+                    }
+                )
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun fetchNotification(call: PluginCall) {
+        val id = call.getString("id") ?: run {
+            call.reject("Missing 'id' parameter.")
+            return
+        }
+
+        Notificare.fetchNotification(id, object : NotificareCallback<NotificareNotification> {
+            override fun onSuccess(result: NotificareNotification) {
+                call.resolve(
+                    JSObject().apply {
+                        put("result", result.toJson())
+                    }
+                )
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    // endregion
+
+    // region Notificare device module
+
+    @PluginMethod
+    public fun getCurrentDevice(call: PluginCall) {
+        call.resolve(
+            JSObject().apply {
+                put("result", Notificare.device().currentDevice?.toJson())
+            }
+        )
+    }
+
+    @PluginMethod
+    public fun getPreferredLanguage(call: PluginCall) {
+        call.resolve(
+            JSObject().apply {
+                put("result", Notificare.device().preferredLanguage)
+            }
+        )
+    }
+
+    @PluginMethod
+    public fun updatePreferredLanguage(call: PluginCall) {
+        val language = call.getString("language") ?: run {
+            call.reject("Missing 'language' parameter.")
+            return
+        }
+
+        Notificare.device().updatePreferredLanguage(language, object : NotificareCallback<Unit> {
+            override fun onSuccess(result: Unit) {
+                call.resolve()
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun register(call: PluginCall) {
+        val userId = call.getString("userId") ?: run {
+            call.reject("Missing 'userId' parameter.")
+            return
+        }
+
+        val userName = call.getString("userName") ?: run {
+            call.reject("Missing 'userName' parameter.")
+            return
+        }
+
+        Notificare.device().register(userId, userName, object : NotificareCallback<Unit> {
+            override fun onSuccess(result: Unit) {
+                call.resolve()
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun fetchTags(call: PluginCall) {
+        Notificare.device().fetchTags(object : NotificareCallback<List<String>> {
+            override fun onSuccess(result: List<String>) {
+                call.resolve(
+                    JSObject().apply {
+                        put("result", JSArray(result))
+                    }
+                )
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun addTag(call: PluginCall) {
+        val tag = call.getString("tag") ?: run {
+            call.reject("Missing 'tag' parameter.")
+            return
+        }
+
+        Notificare.device().addTag(tag, object : NotificareCallback<Unit> {
+            override fun onSuccess(result: Unit) {
+                call.resolve()
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun addTags(call: PluginCall) {
+        val tags = call.getArray("tags", null)?.toList<String>() ?: run {
+            call.reject("Missing 'tags' parameter.")
+            return
+        }
+
+        Notificare.device().addTags(tags, object : NotificareCallback<Unit> {
+            override fun onSuccess(result: Unit) {
+                call.resolve()
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun removeTag(call: PluginCall) {
+        val tag = call.getString("tag") ?: run {
+            call.reject("Missing 'tag' parameter.")
+            return
+        }
+
+        Notificare.device().removeTag(tag, object : NotificareCallback<Unit> {
+            override fun onSuccess(result: Unit) {
+                call.resolve()
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun removeTags(call: PluginCall) {
+        val tags = call.getArray("tags", null)?.toList<String>() ?: run {
+            call.reject("Missing 'tags' parameter.")
+            return
+        }
+
+        Notificare.device().removeTags(tags, object : NotificareCallback<Unit> {
+            override fun onSuccess(result: Unit) {
+                call.resolve()
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun clearTags(call: PluginCall) {
+        Notificare.device().clearTags(object : NotificareCallback<Unit> {
+            override fun onSuccess(result: Unit) {
+                call.resolve()
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun fetchDoNotDisturb(call: PluginCall) {
+        Notificare.device().fetchDoNotDisturb(object : NotificareCallback<NotificareDoNotDisturb?> {
+            override fun onSuccess(result: NotificareDoNotDisturb?) {
+                call.resolve(
+                    JSObject().apply {
+                        put("result", result?.toJson())
+                    }
+                )
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun updateDoNotDisturb(call: PluginCall) {
+        val dnd = call.getObject("dnd", null)?.let { NotificareDoNotDisturb.fromJson(it) } ?: run {
+            call.reject("Missing 'dnd' parameter.")
+            return
+        }
+
+        Notificare.device().updateDoNotDisturb(dnd, object : NotificareCallback<Unit> {
+            override fun onSuccess(result: Unit) {
+                call.resolve()
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun clearDoNotDisturb(call: PluginCall) {
+        Notificare.device().clearDoNotDisturb(object : NotificareCallback<Unit> {
+            override fun onSuccess(result: Unit) {
+                call.resolve()
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun fetchUserData(call: PluginCall) {
+        Notificare.device().fetchUserData(object : NotificareCallback<NotificareUserData> {
+            override fun onSuccess(result: NotificareUserData) {
+                val userData = JSONObject().apply {
+                    result.forEach {
+                        put(it.key, it.value)
+                    }
+                }
+
+                call.resolve(
+                    JSObject().apply {
+                        put("result", userData)
+                    }
+                )
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    @PluginMethod
+    public fun updateUserData(call: PluginCall) {
+        val json = call.getObject("userData", null) ?: run {
+            call.reject("Missing 'userData' parameter.")
+            return
+        }
+
+        val userData: NotificareUserData = mutableMapOf<String, String>().apply {
+            val iterator = json.keys()
+            while (iterator.hasNext()) {
+                val key = iterator.next()
+                val value = json.getString(key)
+
+                if (value != null) {
+                    put(key, value)
+                }
+            }
+        }
+
+        Notificare.device().updateUserData(userData, object : NotificareCallback<Unit> {
+            override fun onSuccess(result: Unit) {
+                call.resolve()
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    // endregion
+
+    // region Notificare events module
+
+    @PluginMethod
+    public fun logCustom(call: PluginCall) {
+        val event = call.getString("event") ?: run {
+            call.reject("Missing 'event' parameter.")
+            return
+        }
+
+        val data: NotificareEventData?
+
+        try {
+            data = call.getObject("data", null)?.let { NotificareEvent.createData(it) }
+        } catch (e: Exception) {
+            call.reject(e.localizedMessage)
+            return
+        }
+
+        Notificare.events().logCustom(event, data, object : NotificareCallback<Unit> {
+            override fun onSuccess(result: Unit) {
+                call.resolve()
+            }
+
+            override fun onFailure(e: Exception) {
+                call.reject(e.localizedMessage)
+            }
+        })
+    }
+
+    // endregion
 }
---
+++ packages/capacitor-notificare/android/src/main/java/re/notifica/capacitor/NotificarePluginIntentReceiver.kt
@@ -0,0 +1,30 @@
+package re.notifica.capacitor
+
+import android.content.Context
+import re.notifica.NotificareIntentReceiver
+import re.notifica.internal.NotificareLogger
+import re.notifica.models.NotificareApplication
+import re.notifica.models.NotificareDevice
+
+internal class NotificarePluginIntentReceiver : NotificareIntentReceiver() {
+
+    override fun onReady(context: Context, application: NotificareApplication) {
+        try {
+            EventBroker.dispatchEvent("ready", application.toJson())
+        } catch (e: Exception) {
+            NotificareLogger.error("Failed to emit the ready event.", e)
+        }
+    }
+
+    override fun onUnlaunched(context: Context) {
+        EventBroker.dispatchEvent("unlaunched", null)
+    }
+
+    override fun onDeviceRegistered(context: Context, device: NotificareDevice) {
+        try {
+            EventBroker.dispatchEvent("device_registered", device.toJson())
+        } catch (e: Exception) {
+            NotificareLogger.error("Failed to emit the device_registered event.", e)
+        }
+    }
+}
