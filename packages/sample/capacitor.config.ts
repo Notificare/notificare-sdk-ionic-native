5e70af297ece19f3b7c5d228c79808538ff1b403
Helder Pinhal <helder@notifica.re>
2022-01-26 15:50:08 +0000

    chore: update appId

--- packages/sample/capacitor.config.ts
+++ packages/sample/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 're.notifica.demo',
   appName: 'sample',
   webDir: 'build',
   bundledWebRuntime: false,
