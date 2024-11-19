886a375241381dc582d1a9d52aeafc1ceae168d1
yevheniismirnov <yevhenii.smirnov@outlook.com>
2024-11-19 12:04:44 +0000

    chore(sample-user-inbox): update to ionic v8

--- packages/sample-user-inbox/src/app.tsx
+++ packages/sample-user-inbox/src/app.tsx
@@ -30,10 +30,23 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* import '@ionic/react/css/palettes/dark.always.css'; */
+/* import '@ionic/react/css/palettes/dark.class.css'; */
+import '@ionic/react/css/palettes/dark.system.css';
+
 /* Theme variables */
 import './theme/variables.css';
 
-setupIonicReact();
+setupIonicReact({
+  innerHTMLTemplatesEnabled: true,
+});
 
 export const App: FC = () => {
   const { isLoading } = useAuth0();
